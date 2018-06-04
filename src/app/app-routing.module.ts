262c8802f6a3ae283f88c120d66489c3685a1d66
khemarachhe <khemarachhe@gmail.com>
2018-06-04 11:14:34 +0700

    errorPopUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponentBodyComponent } from './app-component-body/app-component-body.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  { path: '', component:  AppComponentBodyComponent },
+  { path: 'thank-you', component:  ThankyouComponent }
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+}
