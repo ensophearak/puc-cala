5814faaa1fc1f4db0fb14a4b7b48398a76a63da9
khemarachhe <khemara@Khemaras-MacBook-Pro.local>
2018-07-14 22:25:26 +0700

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  ABA_PAYWAY_API_URL:'https://payway-dev.ababank.com/api/pwpucp/',
-  ABA_PAYWAY_API_KEY:'1be5670f9087eb7a33a67a1e71ddba72',
-  ABA_PAYWAY_MERCHANT_ID:'puc',
-  // ABA_PAYWAY_API_URL:'https://payway.ababank.com/api/pwpannasastrapuniversityucambodiac/',
-  // ABA_PAYWAY_API_KEY:'139e5514955e4fc5266bcd0447663937',
-  // ABA_PAYWAY_MERCHANT_ID:'pannasastrauniversitycambodia',
+  // ABA_PAYWAY_API_URL:'https://payway-dev.ababank.com/api/pwpucp/',
+  // ABA_PAYWAY_API_KEY:'1be5670f9087eb7a33a67a1e71ddba72',
+  // ABA_PAYWAY_MERCHANT_ID:'puc',
+  ABA_PAYWAY_API_URL:'https://payway.ababank.com/api/pwpannasastrapuniversityucambodiac/',
+  ABA_PAYWAY_API_KEY:'139e5514955e4fc5266bcd0447663937',
+  ABA_PAYWAY_MERCHANT_ID:'pannasastrauniversitycambodia',
   firebase: {
     apiKey: "AIzaSyDNR_RhQ9meMX1DJ57djE0D7Zq40zFaK6s",
     authDomain: "puccala-b98b9.firebaseapp.com",
